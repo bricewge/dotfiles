f97c6e03c4c541c03fbb45bbc79578c6d419dbda
Brice Waegeneire <brice.wge@gmail.com>
2015-12-02 15:48:38 +0100

    zsh: set firefox as my browser, init.sh: prettify the output

--- init.sh
+++ init.sh
@@ -11,7 +11,7 @@ if file "$fileroot/ssh/init.sls" | grep -q text$; then
     decrypted=True
 fi
 
-echo "Running as '$user' with home directory '$home'..."
+echo -e "Running as '\033[1m$user\033[0m' with home directory '\033[1m$home\033[0m'..."
 
 # Apply the dotfiles to your home
 sudo salt-call                                      \
