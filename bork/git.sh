1b4ed056b031bbe6cb345eed9686d070461dd2f4
Brice Waegeneire <brice.wge@gmail.com>
2017-09-17 15:23:31 +0200

    git: update bork script and use upstream git-fire

--- bork/git.sh
+++ bork/git.sh
@@ -1,10 +1,10 @@
-dotfiles="$HOME/.dotfiles"
+include config
 
-ok symlink ~/.gitconfig $dotfiles/git/config
-ok symlink ~/.gitignore $dotfiles/git/ignore
-ok github ~/.local/src/git-fire "bricewge/git-fire"
+ok symlink ~/.gitconfig "${dotfiles}/git/config"
+ok symlink ~/.gitignore "${dotfiles}/git/ignore"
+ok github ~/.local/src/git-fire "qw3rtman/git-fire"
 ok symlink ~/.local/bin/git-fire ~/.local/src/git-fire/git-fire
-if [[ $OS="macos" ]];then
+if [[ $OS == "macos" ]]; then
     ok brew git-crypt
     ok brew git-subrepo
 fi
