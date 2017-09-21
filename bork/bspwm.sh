f7f67b12f889611996d8b68203cf75e59b57d08e
Brice Waegeneire <brice.wge@gmail.com>
2017-09-22 01:26:54 +0200

    bspwm: draft for wm config

--- bork/bspwm.sh
+++ bork/bspwm.sh
@@ -1,4 +1,11 @@
-dotfiles=$HOME/.dotfiles
+include config
 
-ok symlink $HOME/.xinitrc $dotfiles/bspwm/xinitrc
-ok symlink $HOME/.Xresources $dotfiles/bspwm/Xresources
+ok symlink $HOME/.xinitrc "${dotfiles}/bspwm/xinitrc"
+ok symlink $HOME/.Xresources "${dotfiles}/bspwm/Xresources"
+ok symlink $HOME/.xsession "${dotfiles}/bspwm/xsession"
+
+ok directory ~/.config
+ok symlink ~/.config/compton.conf "${dotfiles}/bspwm/compton.conf"
+
+ok symlink ~/.config/polybar "${dotfiles}/polybar"
+ok symlink ~/.config/polybar/colors "${dotfiles}/polybar/colors-default"
