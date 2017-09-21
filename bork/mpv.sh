f97c5ef8060e335f5c847067ee8e20a2fdd17724
Brice Waegeneire <brice.wge@gmail.com>
2017-09-22 01:18:56 +0200

    mpv: add config
    Signed-off-by: Brice Waegeneire <brice.wge@gmail.com>

--- bork/mpv.sh
+++ bork/mpv.sh
@@ -1,4 +1,4 @@
-dotfiles=$HOME/.dotfiles
+include config
 
-ok symlink $HOME/.config/mpv/input.conf $dotfiles/mpv/input.conf
-ok symlink $HOME/.config/mpv/mpv.conf $dotfiles/mpv/mpv.conf
+ok symlink "${HOME}/.config/mpv/input.conf" "${dotfiles}/mpv/input.conf"
+ok symlink "${HOME}/.config/mpv/mpv.conf" "${dotfiles}/mpv/mpv.conf"
