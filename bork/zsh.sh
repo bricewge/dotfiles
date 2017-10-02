3fbd932d3485f4e8a4a2ec1ab087b86a73be02d4
Brice Waegeneire <brice.wge@gmail.com>
2017-10-02 10:34:56 +0200

    zsh: update borkfile, disable macOS' PIV popup, minor fixes

--- bork/zsh.sh
+++ bork/zsh.sh
@@ -1,5 +1,8 @@
-dotfiles="$HOME/.dotfiles"
+include config
 
-ok symlink ~/.zshrc $dotfiles/zsh/zshrc
-ok symlink ~/.zshenv $dotfiles/zsh/zshenv
-ok symlink ~/.zpreztorc $dotfiles/zsh/zpreztorc
+if [[ $OS == "macos" ]]; then
+    ok brew-tap homebrew/command-not-found
+fi
+ok symlink ~/.zshrc "${dotfiles}/zsh/zshrc"
+ok symlink ~/.zshenv "${dotfiles}/zsh/zshenv"
+ok symlink ~/.zpreztorc "${dotfiles}/zsh/zpreztorc"
