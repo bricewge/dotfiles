615c76a8cf0869853ad8ae92d9ab50b186316a24
Brice Waegeneire <brice.wge@gmail.com>
2017-10-19 23:08:20 +0200

    bspwm, polybar: update config

--- bork/bspwm.sh
+++ bork/bspwm.sh
@@ -1,11 +1,16 @@
 include config
 
-ok symlink $HOME/.xinitrc "${dotfiles}/bspwm/xinitrc"
-ok symlink $HOME/.Xresources "${dotfiles}/bspwm/Xresources"
-ok symlink $HOME/.xsession "${dotfiles}/bspwm/xsession"
+ok directory ${HOME}/.config/{bspwm,polybar,sxhkd}
 
-ok directory ~/.config
-ok symlink ~/.config/compton.conf "${dotfiles}/bspwm/compton.conf"
+ok symlink "${HOME}/.xinitrc" "${dotfiles}/bspwm/xinitrc"
+ok symlink "${HOME}/.Xresources" "${dotfiles}/bspwm/Xresources"
+ok symlink "${HOME}/.xsession" "${dotfiles}/bspwm/xsession"
 
-ok symlink ~/.config/polybar "${dotfiles}/polybar"
-ok symlink ~/.config/polybar/colors "${dotfiles}/polybar/colors-default"
+ok symlink ${HOME}/.config/compton.conf "${dotfiles}/bspwm/compton.conf"
+
+ok symlink ${HOME}/.config/polybar "${dotfiles}/polybar"
+ok symlink ${HOME}/.config/polybar/colors "${dotfiles}/polybar/colors-default"
+
+ok symlink "${HOME}/.config/bspwm/bspwmrc" "${dotfiles}/bspwm/bspwmrc"
+ok symlink "${HOME}/.config/bspwm/env" "${dotfiles}/bspwm/env"
+ok symlink "${HOME}/.config/sxhkd/sxhkdrc" "${dotfiles}/bspwm/sxhkdrc"
