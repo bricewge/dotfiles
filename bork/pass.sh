cd4e18f72fe3357d61ee7bc969e694fda9d97fe7
Brice Waegeneire <brice.wge@gmail.com>
2017-10-28 12:22:31 +0200

    bork: pass test if the hook is executable

--- bork/pass.sh
+++ bork/pass.sh
@@ -4,7 +4,7 @@ if pass help >/dev/null 2>&1 ; then
     if [ ! -d "${HOME}/.password-store" ]; then
         pass git clone git@github.com:bricewge/pass.git
     fi
-    if [ ! -f  "${HOME}/.password-store/.git/hooks/post-commit" ]; then
+    if [ ! -x "${HOME}/.password-store/.git/hooks/post-commit" ]; then
         cat > "${HOME}/.password-store/.git/hooks/post-commit" <<-EOF
 				git pull --rebase
 				git push origin
