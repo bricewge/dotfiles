ec6fcd04704188a7314857f26e7fe70cbe2c97b9
Brice Waegeneire <brice.wge@gmail.com>
2017-12-01 18:11:53 +0100

    zsh: add zprofile, source zshenv one time and install notifer

--- bork/zsh.sh
+++ bork/zsh.sh
@@ -2,7 +2,9 @@ include config
 
 if [[ $OS == "macos" ]]; then
     ok brew-tap homebrew/command-not-found
+    ok brew terminal-notifier
 fi
 ok symlink ~/.zshrc "${dotfiles}/zsh/zshrc"
 ok symlink ~/.zshenv "${dotfiles}/zsh/zshenv"
 ok symlink ~/.zpreztorc "${dotfiles}/zsh/zpreztorc"
+ok symlink ~/.zprofile "${dotfiles}/zsh/zprofile"
