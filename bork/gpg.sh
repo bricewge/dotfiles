a21f6aa5c0b7105271277c58c6ea00bd221c58b5
Brice Waegeneire <brice.wge@gmail.com>
2017-12-02 11:16:35 +0100

    gpg: use pinentry-mac by default

--- bork/gpg.sh
+++ bork/gpg.sh
@@ -3,13 +3,18 @@ include config
 if ok directory ~/.gnupg; then
     chmod 700 ~/.gnupg
 fi
-ok symlink ~/.gnupg/gpg-agent.conf "${dotfiles}/gpg/gpg-agent.conf"
 if [[ $OS == "macos" ]]; then
     ok brew gnupg
     ok brew pinentry-mac
+    # Since hombrew maintainers won't set pinentry-mac as default, let's get dirty!
+    # https://github.com/Homebrew/homebrew-core/issues/18011
+    cp -f "${dotfiles}/gpg/gpg-agent.conf" "${HOME}/.gnupg/" && \
+        echo "pinentry-program /Users/bwaegene/.brew/bin/pinentry-mac" >> "${HOME}/.gnupg/gpg-agent.conf"
     ok symlink ~/Library/LaunchAgents/bricewge.gpg-agent.plist "${dotfiles}/gpg/gpg-agent.plist"
 	  launchctl load ~/.dotfiles/gpg/gpg-agent.plist
 	  launchctl start bricewge.gpg-agent
+else
+    ok symlink ~/.gnupg/gpg-agent.conf "${dotfiles}/gpg/gpg-agent.conf"
 fi
 # Import personal public key
 if ! gpg --list-keys 3D36CAA0116F0F99 >/dev/null 2>&1; then
