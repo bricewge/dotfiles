fad43fbf9a254bbfa69212ef3b8e8561a3ca0b79
Brice Waegeneire <brice.wge@gmail.com>
2018-04-29 20:13:41 +0200

    add script to create file hierarchy

---
+++ home.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+cd ~
+mkdir -p \
+      42 \
+      misc/{iso,email,vpn,paperasse} \
+      mnt \
+      org \
+      project \
+      video \
+      game \
+      public \
+      picture \
+      music \
+      vm
+cd -
