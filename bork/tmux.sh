98d6460de33acf3fb428e0a7142fbdd7e28e7944
Brice Waegeneire <brice.wge@gmail.com>
2018-04-29 20:12:59 +0200

    tmux: bork install plugins

--- bork/tmux.sh
+++ bork/tmux.sh
@@ -3,3 +3,4 @@ include config
 ok directory "${HOME}/.tmux/plugins"
 ok github "${HOME}/.tmux/plugins/tpm" tmux-plugins/tpm
 ok symlink "${HOME}/.tmux.conf" "${dotfiles}/tmux/config"
+"${HOME}/.tmux/plugins/tpm/bin/install_plugins"
