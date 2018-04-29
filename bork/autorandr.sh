ba632c8f6e2b204bfb1e8dde4f4e3916c0c6df07
Brice Waegeneire <brice.wge@gmail.com>
2018-04-29 12:57:49 +0200

    add autorandr config

---
+++ bork/autorandr.sh
@@ -0,0 +1,5 @@
+include config
+
+ok directory "${HOME}/.config/autorandr/docked"
+ok symlink "${HOME}/.config/autorandr/docked/postswitch" "${dotfiles}/autorandr/docked-postswitch"
+ok symlink "${HOME}/.config/autorandr/settings.ini" "${dotfiles}/autorandr/settings.ini"
