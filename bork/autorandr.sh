ae99cd5b74a749f51bc1360fc86019ad14b9850f
Brice Waegeneire <brice.wge@gmail.com>
2018-05-09 16:54:50 +0200

    autorandr: split postswitch script

--- bork/autorandr.sh
+++ bork/autorandr.sh
@@ -1,5 +1,6 @@
 include config
 
 ok directory "${HOME}/.config/autorandr/docked"
-ok symlink "${HOME}/.config/autorandr/docked/postswitch" "${dotfiles}/autorandr/docked-postswitch"
+ok symlink "${HOME}/.config/autorandr/docked/postswitch" "${dotfiles}/autorandr/docked/postswitch"
+ok symlink "${HOME}/.config/autorandr/postswitch.d" "${dotfiles}/autorandr/postswitch.d"
 ok symlink "${HOME}/.config/autorandr/settings.ini" "${dotfiles}/autorandr/settings.ini"
