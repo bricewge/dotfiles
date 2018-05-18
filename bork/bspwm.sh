4acc6957ce2e2ea6e6cde59bb62da660589b3da3
Brice Waegeneire <brice.wge@gmail.com>
2018-05-18 16:02:37 +0200

    add mimeo in place of xdg-open

--- bork/bspwm.sh
+++ bork/bspwm.sh
@@ -1,16 +1,18 @@
 include config
 
-ok directory ${HOME}/.config/bspwm
-ok directory ${HOME}/.config/sxhkd
-
 ok symlink "${HOME}/.xinitrc" "${dotfiles}/bspwm/xinitrc"
 ok symlink "${HOME}/.Xresources" "${dotfiles}/bspwm/Xresources"
 ok symlink "${HOME}/.xsession" "${dotfiles}/bspwm/xsession"
 
-ok symlink ${HOME}/.config/compton.conf "${dotfiles}/bspwm/compton.conf"
+ok symlink "${HOME}/.config/compton.conf" "${dotfiles}/bspwm/compton.conf"
 
-ok symlink ${HOME}/.config/polybar "${dotfiles}/polybar"
+ok symlink "${HOME}/.config/polybar" "${dotfiles}/polybar"
 
+ok directory "${HOME}/.config/mimeo"
+ok symlink "${HOME}/.config/mimeo/associations.txt" "${dotfiles}/bspwm/mimeo.conf"
+
+ok directory "${HOME}/.config/bspwm"
 ok symlink "${HOME}/.config/bspwm/bspwmrc" "${dotfiles}/bspwm/bspwmrc"
 ok symlink "${HOME}/.config/bspwm/env" "${dotfiles}/bspwm/env"
+ok directory "${HOME}/.config/sxhkd"
 ok symlink "${HOME}/.config/sxhkd/sxhkdrc" "${dotfiles}/bspwm/sxhkdrc"
