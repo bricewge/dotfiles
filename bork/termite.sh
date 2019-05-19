291fc00ebaef83a860b3fddf72fdcf8faeb6dce9
Brice Waegeneire <brice.wge@gmail.com>
2019-05-19 19:24:18 +0200

    termite: remove termite
    alacritty is my new default terminal

--- bork/termite.sh
+++
@@ -1,6 +0,0 @@
-include config
-
-ok directory "${HOME}/.config/termite"
-ok symlink "${HOME}/.config/termite/dark" "${dotfiles}/termite/dark"
-ok symlink "${HOME}/.config/termite/light" "${dotfiles}/termite/light"
-ok symlink "${HOME}/.config/termite/config" "${HOME}/.config/termite/dark"
