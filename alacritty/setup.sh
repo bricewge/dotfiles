0b9a0c1c17d6053190d65727509e9d79039cce6d
Brice Waegeneire <brice.wge@gmail.com>
2019-05-25 18:06:54 +0200

    alacritty: clean up

--- alacritty/setup.sh
+++ alacritty/setup.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
+# WAITING emacs 27 won't need xterm-24 bit anymore
 tic -x -o ~/.terminfo ./_alacritty.terminfo
--- bork/alacritty.sh
+++
@@ -1,6 +0,0 @@
-include config
-
-ok directory "${HOME}/.config/alacritty"
-ok symlink "${HOME}/.config/alacritty/alacritty.yml" "${dotfiles}/alacritty/alacritty.yml"
-ok directory "${HOME}/.terminfo/x/"
-ok symlink "${HOME}/.terminfo/x/xterm-24bit" "${dotfiles}/alacritty/xterm-24bit"
