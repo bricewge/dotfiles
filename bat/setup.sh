3a47aeb5b273460096c9aadedcf830691622632c
Brice Waegeneire <brice.wge@gmail.com>
2019-05-25 20:08:57 +0200

    bat: add org-mode support

--- bat/setup.sh
+++ bat/setup.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env sh
 
 config_dir=$(bat --config-dir)
-mkdir -p "$config_dir/{syntaxes,themes}"
 
 # * themes
+mkdir -p "$config_dir/themes"
 git clone https://github.com/paulcpederson/solarized-sublime.git \
   "$config_dir/themes/solarized"
+
+# * syntaxes
+mkdir -p "$config_dir/syntaxes"
+git clone https://gist.github.com/25edc5f66404dee2fe19bf90c205a29c.git \
+  "$config_dir/syntaxes/org-mode"
 
 bat cache --build
