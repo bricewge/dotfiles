e0ccaa592e3d988f890fc6ee26c24e5b755f7473
Brice Waegeneire <brice.wge@gmail.com>
2019-06-08 21:56:08 +0200

    mpv: migrate to new organization

--- bork/mpv.sh
+++
@@ -1,8 +0,0 @@
-include config
-
-if [[ $OS == "macos" ]]; then
-    ok cask mpv "${appdir}"
-fi
-ok directory "${HOME}/.config/mpv"
-ok symlink "${HOME}/.config/mpv/input.conf" "${dotfiles}/mpv/input.conf"
-ok symlink "${HOME}/.config/mpv/mpv.conf" "${dotfiles}/mpv/mpv.conf"
---
+++ mpv/setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+config_dir=$XDG_CONFIG_HOME/mpv
+mkdir -p "$config_dir/scripts"
+
+mpv_thumbnail_dir=$HOME/.local/src/mpv_thumbnail_script
+if [ ! -d "$mpv_thumbnail_dir" ]; then
+    mkdir -p "$mpv_thumbnail_dir"
+    git clone https://github.com/TheAMM/mpv_thumbnail_script.git \
+        "$mpv_thumbnail_dir"
+fi
+# Upstream generate those files with python and make. Let's do the same the UNIX
+# way, with our trust cat.
+(
+    cd "$mpv_thumbnail_dir" || exit 1
+    cat "lib/helpers.lua" "lib/sha1.lua" "src/options.lua" \
+        "src/thumbnailer_shared.lua" "src/patched_osc.lua" \
+        > "$config_dir/scripts/mpv_thumbnail_script_client_osc.lua"
+    cat "lib/helpers.lua" "src/options.lua" "src/thumbnailer_server.lua" \
+        > "$config_dir/scripts/mpv_thumbnail_script_server.lua"
+)
