179af5273d737c901006eeca9239c9a9f6d39910
Brice Waegeneire <brice.wge@gmail.com>
2019-06-12 22:47:17 +0200

    autorandr: migrate to new organization

---
+++ autorandr/.config/autorandr/postswitch.d/polybars.sh
@@ -0,0 +1,1 @@
+../../../../polybar/.local/bin/polybars.sh
--- bork/autorandr.sh
+++ bork/autorandr.sh
@@ -1,8 +1,1 @@
-include config
-
-ok directory "${HOME}/.config/autorandr/docked"
-ok directory "${HOME}/.config/autorandr/postswitch.d"
-
-ok symlink "${HOME}/.config/autorandr/docked/postswitch" "${dotfiles}/autorandr/docked/postswitch"
-ok symlink "${HOME}/.config/autorandr/postswitch.d/polybars.sh" "${dotfiles}/bin/polybars.sh"
-ok symlink "${HOME}/.config/autorandr/settings.ini" "${dotfiles}/autorandr/settings.ini"
+#!/usr/bin/env bash
