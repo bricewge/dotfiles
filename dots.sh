b5f08675c45659de52a590bed00448d756f901e4
Brice Waegeneire <brice.wge@gmail.com>
2019-06-13 15:40:24 +0200

    merge remanent install scripts in dots.sh

--- dots.sh
+++ dots.sh
@@ -7,6 +7,40 @@
 
 set -e
 
+home_hierarchy() {
+    (
+            cd "$HOME"
+            mkdir -p \
+                42 \
+                download \
+                game \
+                misc/iso misc/email misc/vpn misc/paperasse \
+                misc/model misc/desktop misc/document\
+                mnt \
+                music \
+                org \
+                picture/screenshot picture/wallpaper \
+                project \
+                sharing \
+                stream \
+                video \
+                vm
+
+            mkdir -p \
+                "${XDG_CONFIG_HOME}" \
+                "${HOME}/.local/src"
+    )
+}
+
+# apply state specific to me as a user
+bricewge() {
+    if [ "$USER" != "bricewge" ] && [ "$USER" != "bwaegene" ]; then return; fi
+
+    home_hierarchy
+    git -C "$DOTFILES" remote set-url origin \
+        git@github.com:bricewge/dotfiles.git
+}
+
 # TODO No arguments: install all the packages
 if [ $# -eq 0 ]; then
     echo "Use with no arguments is not yet implemented"
@@ -17,10 +51,14 @@ fi
 dependencies="git stow curl"
 for dep in ${dependencies}; do
     command -v "$dep" 1>/dev/null || \
-        (printf "dependenciy '%s' is missing, please install it\n" "$dep" ; \
+        (printf "dependency '%s' is missing, please install it\n" "$dep" ; \
         exit 1)
 done
 
+
+
+# TODO Replace $0 because it's unreliable
+# https://mywiki.wooledge.org/BashFAQ/028
 cd "$(dirname "$0")" || exit 1
 DOTFILES=$PWD
 export DOTFILES
@@ -29,6 +67,8 @@ printf "export DOTFILES=%s" "$DOTFILES" > "$HOME/.shell/profile/dotfiles"
 
 # TODO Source shell env and libs
 
+bricewge
+
 # Arguments: limit the install to those packages
 stow --target "$HOME" "$@"
 for pkg in "$@"; do
@@ -36,6 +76,6 @@ for pkg in "$@"; do
             cd "$pkg" || exit 1
             # shellcheck disable=SC1091
             . ./setup.sh
-            # TODO execute OS subpacakges setup
+            # TODO execute OS subpackages setup
     ) fi
 done
