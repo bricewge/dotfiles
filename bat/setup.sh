2dbfd95a3c97eccc6cf36734ec32f27fbfb9e401
Brice Waegeneire <brice.wge@gmail.com>
2019-06-19 15:25:24 +0200

    bat: add comment

--- bat/setup.sh
+++ bat/setup.sh
@@ -8,6 +8,7 @@ git clone https://github.com/paulcpederson/solarized-sublime.git \
   "$config_dir/themes/solarized"
 
 # * syntaxes
+# WAITING For release of bat > 0.11.0
 mkdir -p "$config_dir/syntaxes"
 git clone https://gist.github.com/25edc5f66404dee2fe19bf90c205a29c.git \
   "$config_dir/syntaxes/org-mode"
