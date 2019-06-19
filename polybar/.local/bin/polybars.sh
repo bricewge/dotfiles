0219042d081508a66be4a67fc162bd334e154cd1
Brice Waegeneire <brice.wge@gmail.com>
2019-06-19 16:32:58 +0200

    polybar: reafactor polybars.sh

--- polybar/.local/bin/polybars.sh
+++ polybar/.local/bin/polybars.sh
@@ -3,18 +3,20 @@
 # This script setup bspwm desktops and theirs status bar. It can be used
 # by itself or as a postswitch hook for autorandr.
 
-if [[ -n ${AUTORANDR_MONITORS} ]]; then
-    monitors=${AUTORANDR_MONITORS}
+set -x
+
+if [ -n "$AUTORANDR_MONITORS" ]; then
+    monitors=$(printf %s "$AUTORANDR_MONITORS" | tr ':' "\n")
 else
-    monitors=$(xrandr --query | awk '/ connected / {print $1}')
+    monitors=$(xrandr --query | awk '$2 ~ /^connected/ {print $1}')
 fi
 
-for monitor in ${monitors//:/ }
-do
-    if [[ "${monitor}" == "LVDS1" ]]; then
-        bspc monitor "${monitor}" -d I II III IV V
+for monitor in ${monitors}; do
+    # TODO Remove handwritten monitor names
+    if [ "$monitor" = "LVDS1" ]; then
+        bspc monitor "$monitor" -d I II III IV V
     else
-        bspc monitor "${monitor}" -d 1 2 3 4 5
+        bspc monitor "$monitor" -d 1 2 3 4 5
     fi
-    systemctl --user start "polybar@${monitor}"
+    systemctl --user start "polybar@${monitor}.service"
 done
