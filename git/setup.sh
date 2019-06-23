d41cd0271b8b80007fa06e9b27b7ee8386d13217
Brice Waegeneire <brice.wge@gmail.com>
2019-06-23 16:44:46 +0200

    git: add a WAITING

--- git/setup.sh
+++ git/setup.sh
@@ -9,7 +9,7 @@ if [ ! -d "$git_fire_dir" ]; then
     ln -s "$git_fire_dir/git-fire" "$git_fire_dir"
 fi
 
-# Workaround for https://github.com/aspiers/stow/issues/3
+# WAITING https://github.com/aspiers/stow/issues/3
 template_dir=$HOME/.config/git/template
 if [ ! -d "$template_dir" ]; then
     ln -s "$PWD/_template" "$template_dir"
