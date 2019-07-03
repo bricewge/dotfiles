34760fec115b08e64e84e6803a6f65dc8dd338ab
Brice Waegeneire <brice.wge@gmail.com>
2019-07-04 00:09:04 +0200

    git: fix symlink to git-fire binary

--- git/setup.sh
+++ git/setup.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-git_fire_dir="${HOME}/.local/src/git-fire"
+git_fire_dir="$HOME/.local/src/git-fire"
 if [ ! -d "$git_fire_dir" ]; then
     mkdir -p "$git_fire_dir"
     git clone https://github.com/qw3rtman/git-fire.git "$git_fire_dir"
-    ln -s "$git_fire_dir/git-fire" "$git_fire_dir"
+    ln -s "$HOME/.local/bin/git-fire" "$git_fire_dir"
 fi
 
 # WAITING https://github.com/aspiers/stow/issues/3
