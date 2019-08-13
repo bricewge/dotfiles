5ef35bc3a176b2a2080fbc4a2f136ee7eaa2ad90
Brice Waegeneire <brice.wge@gmail.com>
2019-08-13 07:23:49 +0200

    bspwm: disable chromium decorations

--- bspwm/setup.sh
+++ bspwm/setup.sh
@@ -3,3 +3,10 @@
 if [ ! -e "$HOME/.config/polybar/colors" ]; then
     ln -s ./solarized-dark "$HOME/.config/polybar/colors"
 fi
+
+# Disable chromium decorations
+chromium_config=$HOME/.config/chromium/Default/Preferences
+if [ -e "$chromium_config" ]; then
+    jq < "$chromium_config" .browser.custom_chrome_frame=false \
+        > /tmp/tmpfile && mv --force /tmp/tmpfile "$chromium_config"
+fi
