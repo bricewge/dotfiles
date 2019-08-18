70fd0cc2b5be38c3c318bb3b28147bd9ea586cac
Brice Waegeneire <brice.wge@gmail.com>
2019-08-18 18:15:33 +0200

    alacritty: indirect link to config
    Allow toggling theme variant.

--- alacritty/setup.sh
+++ alacritty/setup.sh
@@ -3,3 +3,11 @@
 # WAITING emacs 27 won't need xterm-24 bit anymore
 tic -x -o ~/.terminfo "$DOTFILES/emacs/_xterm-24bits.terminfo"
 tic -x -o ~/.terminfo ./_alacritty.terminfo
+
+# Using an indirect symlink as alacritty's config file allow us to siwtch theme
+# variant without touching of the default value in the dotfiles.
+theme=$(readlink "$XDG_CONFIG_HOME/alacritty/alacritty.yml")
+if [ ! "$(dirname "$theme")" =  "$XDG_CONFIG_HOME/alacritty" ]; then
+    symlink "$XDG_CONFIG_HOME/alacritty/solarized.yml" \
+        "$XDG_CONFIG_HOME/alacritty/alacritty.yml"
+fi
