4f7dc0f184ea2202f413301449c7bdb3748b4f6a
Brice Waegeneire <brice.wge@gmail.com>
2019-08-18 19:39:03 +0200

    bat: fix setup.sh errors

--- bat/setup.sh
+++ bat/setup.sh
@@ -6,13 +6,13 @@ config_dir=$(bat --config-dir)
 mkdir -p "$config_dir/themes"
 # WAITING https://github.com/sharkdp/bat/issues/339
 # git clone https://github.com/paulcpederson/solarized-sublime.git \
-git clone https://github.com/bricewge/solarized-sublime.git \
+repository https://github.com/bricewge/solarized-sublime.git \
   "$config_dir/themes/solarized"
 
 # * syntaxes
 # WAITING For release of bat > 0.11.0
 mkdir -p "$config_dir/syntaxes"
-git clone https://gist.github.com/25edc5f66404dee2fe19bf90c205a29c.git \
+repository https://gist.github.com/25edc5f66404dee2fe19bf90c205a29c.git \
   "$config_dir/syntaxes/org-mode"
 
 bat cache --build
