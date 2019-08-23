28cc9bf8ff0eaf9ded03226ef89afa6aa2437dd5
Brice Waegeneire <brice.wge@gmail.com>
2019-08-23 15:14:56 +0200

    emacs :remove .dir-locals.el
    It isn't needed anymore because `doom-private-dir` is a now a symlink to
    the dotfile's emacs module instead of being a directory containing links
    to the module.

--- emacs/.doom.d/.dir-locals.el
+++
@@ -1,6 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((emacs-lisp-mode . ((flycheck-disabled-checkers . 'emacs-lisp-checkdoc)
-                     (eval . (set (make-local-variable 'flycheck-emacs-lisp-check-form)
-                                  +emacs-lisp-reduced-flycheck-emacs-lisp-check-form-h)))))
