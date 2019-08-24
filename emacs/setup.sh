a52dd579c41a759fbd9becb187db14ca8a979ff3
Brice Waegeneire <brice.wge@gmail.com>
2019-08-24 23:42:53 +0200

    emacs: use doom's straight branch

--- emacs/setup.sh
+++ emacs/setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+set -e
+
 # shellcheck disable=SC1090
 . "${DOTFILES:-.}/shell/.shell/lib/utils.sh"
 
@@ -7,8 +9,7 @@
 tic -x -o "$HOME/.terminfo" "${DOTFILES:-.}/emacs/_xterm-24bits.terminfo"
 
 # doom emacs
-# WAITING https://github.com/hlissner/doom-emacs/pull/1681
 repository https://github.com/hlissner/doom-emacs \
     "$HOME/.emacs.d" \
-    bricewge
-"$HOME/.emacs.d/bin/doom" upgrade
+    straight
+"$HOME/.emacs.d/bin/doom" refresh
