acffe925a7b35cc17f4405183989ec494d80c90c
Brice Waegeneire <brice@waegenei.re>
2019-10-12 22:49:59 +0200

    bspwm: mpvctl.sh get socket path from mpv config

--- bspwm/.local/bin/mpvctl.sh
+++ bspwm/.local/bin/mpvctl.sh
@@ -2,6 +2,7 @@
 #
 # TODO Don't flicker when changing file
 
+mpv_socket=$(awk -F= '/input-ipc-server/ {print $2}'  ~/.config/mpv/mpv.conf)
 ipc_cmd='{ "command": ["observe_property", 1, "filtered-metadata"] }'
 query='select(.data) | .data |
 if (.Artist and .Title) then "\(.Artist) ― \(.Title)"
@@ -13,7 +14,4 @@ print() {
     echo $1 | jq -r "$query"
 }
 
-# First display
-print "$(mpvctl prop filtered-metadata 2>/dev/null)"
-# Update the display
-echo "$ipc_cmd" | nc -U ~/.mpv.socket | while read -r l ; do print "$l"; done
+echo "$ipc_cmd" | nc -U "$mpv_socket" | while read -r l ; do print "$l"; done
