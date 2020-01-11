859e04b46892d5c77f41f8359f160af093785eaf
Brice Waegeneire <brice@waegenei.re>
2020-01-11 11:22:50 +0100

    macos: replace `hostname` with POSIX  `uname -n`

--- @macos/.shell/login.d/macos.sh
+++ @macos/.shell/login.d/macos.sh
@@ -1,6 +1,6 @@
 export PATH=$HOME/.brew/bin:$PATH
 
-if [ "$(hostname | cut -d . -f 2-3)" = "42.fr" ]; then
+if [ "$(uname -n | cut -d . -f 2-3)" = "42.fr" ]; then
     export HOMEBREW_CASK_OPTS='--appdir=~/Applications'
     # Disable constant logging of the terminal
     unset -f precmd
