8684a0793b82a02773980b5366165edba5c22da9
Brice Waegeneire <brice@waegenei.re>
2020-03-09 14:53:54 +0100

    emacs: add package org-ql

--- emacs/.config/doom/modules/private/org/packages.el
+++ emacs/.config/doom/modules/private/org/packages.el
@@ -4,6 +4,7 @@
 (package! org-caldav
   :recipe (:host github :repo "grauschnabel/org-caldav")) ;; VTODO support
 (package! org-noter)  ;; annotate documents
+(package! org-ql)
 
 (package! outshine)
 (package! poporg)
