29801a904f515f11147a5873b9b8a610273b02f1
Brice Waegeneire <brice@waegenei.re>
2020-06-28 10:51:31 +0200

    shepherd: Add 'xsecurelock' service.

--- guix/.config/shepherd/init.scm
+++ guix/.config/shepherd/init.scm
@@ -1,8 +1,8 @@
 (use-modules (srfi srfi-2))
 
 ;; TODO add sxhkd service
-;; TODO maybe add xsecurelock service
-;; TODO fix zombies processes
+;; TODO add tmux
+;; TODO get rid off zombies processes
 
 (define* (strings->symbol #:rest r)
   "Return the symbol whose name is the concatenation of strings."
@@ -66,6 +66,31 @@
    #:actions (make-actions
               (reload "Reload configuration."
                       (make-kill-destructor SIGUSR1))))
+
+ (make <service>
+   #:docstring "Screen locker for X11."
+   #:provides '(xsecurelock xss-lock screen-locker)
+   #:start
+   (make-forkexec-constructor
+    (list "xss-lock" "--transfer-sleep-lock" "--"
+          "envfile"
+          (string-append (getenv "HOME") "/.config/xsecurelock/theme.env") "--"
+          "xsecurelock" "--"
+          "xdotool" "key" "0")
+    ;; TODO write an environment-file procedure
+    ;; '("xss-lock" "--transfer-sleep-lock" "xsecurelock")
+    ;; #:environment-variables
+    ;; ((lambda _
+    ;;    (environment-file (string-append
+    ;;                       (getenv "HOME")
+    ;;                       "/.config/xsecurelock/theme.env"))
+    ;;    (environ)))
+    )
+   #:stop (make-kill-destructor)
+   #:actions (make-actions
+              (reset "Reset the screensaver, if the screen isn't locked yet."
+                     (make-kill-destructor SIGHUP))))
+
  (make <service>
    #:docstring "Automounter for removable media."
    #:provides '(udiskie auto-mount)
@@ -109,6 +134,7 @@
                   polybar
                   picom
                   gpg-agent
+                  xss-lock
                   emacs
                   udiskie
                   mpris-proxy))
