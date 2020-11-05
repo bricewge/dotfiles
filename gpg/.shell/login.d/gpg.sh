d13c85e50b684ee204641f7ff68c7b291d82c53b
Brice Waegeneire <brice@waegenei.re>
2020-11-05 22:11:10 +0100

    gpg: Don't overwrite ssh-agent's forwarding.

--- gpg/.shell/login.d/gpg.sh
+++ gpg/.shell/login.d/gpg.sh
@@ -1,5 +1,7 @@
-unset SSH_AGENT_PID
-if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
-    SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
-    export SSH_AUTH_SOCK
+if [ -z "$SSH_AUTH_SOCK" ]; then
+    unset SSH_AGENT_PID
+    if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+        SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+        export SSH_AUTH_SOCK
+    fi
 fi
