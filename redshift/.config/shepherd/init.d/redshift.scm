f1b5d357d24970a57698e4422074483dab320719
Brice Waegeneire <brice@waegenei.re>
2020-11-23 19:12:19 +0100

    redshift: shepherd: Add reset and toggle actions.

--- redshift/.config/shepherd/init.d/redshift.scm
+++ redshift/.config/shepherd/init.d/redshift.scm
@@ -3,7 +3,14 @@
     #:doctring "Adjust the color temperature of the screen."
     #:provides '(redshift)
     #:start (make-forkexec-constructor '("redshift" "-P"))
-    #:stop (make-kill-destructor SIGKILL)))
+    #:stop (lambda (pid . args)
+             (apply (make-kill-destructor SIGKILL) pid args)
+             (action 'redshift 'reset))
+    #:actions (make-actions
+               (toggle "Pause or resume the color temperature adjustement"
+                       (make-kill-destructor SIGUSR1))
+               (reset "Remove adjustment from screen"
+                      (make-forkexec-constructor '("redshift" "-x"))))))
 
 (register-services redshift)
 
